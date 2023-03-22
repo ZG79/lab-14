21fe9f141f496b2ed982ceeb4322913478efc7cc
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-03-21 22:09:16 -0700

    stringify

--- js/app.js
+++ js/app.js
@@ -20,6 +20,9 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  let stringify = JSON.stringify(state.cart);
+  localStorage.setItem('cart', stringify);
+
 };
 
 Cart.prototype.removeItem = function(item) {
--- js/cart.js
+++ js/cart.js
@@ -15,18 +15,32 @@ function renderCart() {
   loadCart();
   clearCart();
   showCart();
+  console.log(state.cart.items)
 }
 
+let tbody = document.querySelector('tbody');
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {
-  
+  for (let i = 1; i<table.rows.length; i++){
+    tbody.deleteRow(i); 
+  }
 }
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
+  for (let i=0; i<state.cart.items.length; i++){
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    let deleteLink = document.createElement('button');
+    deleteLink.textContent = 'x'
+    td.appendChild(deleteLink);
 
+    let tdOne = document.createElement('td');
+    tdOne.textContent = Cart.items[i].quantity;
+    tbody.appendChild(tr);
+  }
   // TODO: Iterate over the items in the cart
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
